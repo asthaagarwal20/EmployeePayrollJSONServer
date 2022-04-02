d3a1600681d42be1da4a1a15c02a18d3cd9ce45d
unknown <asthaagarwal203@gmail.com>
2022-04-02 10:21:04 +0530

    [Astha] Added: UC1 Retrive All Records Using Rest Api

---
+++ EmployeePayrollJSONServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollJSONServer
+{
+     class Program
+    {
+        public  static List<Employee> getEmployee()
+        {
+            RestClient client = new RestClient("http://localhost:3000");
+            RestRequest request = new RestRequest("/employees",Method.GET);
+            var response =  client.Execute(request);
+            return JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+        }
+        static void Main(string[] args)
+        {
+            var list = getEmployee();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.name);
+            }
+        }
+    }
+}
